f1dc7bb0e94ce9b759b2f47fb97abf99d77fecc5
IshaqYaqublu <ishaqyg@code.edu.az>
2022-04-06 19:45:25 +0400

    first commit

---
+++ asset/js/app.js
@@ -0,0 +1,21 @@
+
+
+
+// function Baku(){
+//     let a = confirm("Salam Aleykum")
+//     if(a == true){
+//         document.body.style.backgroundColor = "blue";  
+//     }
+// }
+// Baku();
+
+// function Baku(){
+//     let age = prompt("Yasinizi daxil edin")
+//     while(age<18){
+//         alert("Yasinizi duzgun daxil edin")
+//         age=prompt("Yasinizi daxil edin")
+//     }
+//         alert("Ugurla daxil oldunuz")
+// }
+// Baku();
+
